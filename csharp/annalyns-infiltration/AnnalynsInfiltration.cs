c2f19ce38bfa1b44910f40147ab6e0391320067b
GilGuereque <gguereque326@gmail.com>
2024-04-29 19:46:50 -0700

    second excercism solution completed

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -4,21 +4,48 @@ static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        // throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        if (knightIsAwake == false)
+        {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        // throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        if (knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true)
+        {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        // throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        if(archerIsAwake == false && prisonerIsAwake == true)
+        {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        // throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if (petDogIsPresent == true && archerIsAwake == false)
+        {
+            return true;
+        } else if (petDogIsPresent != true && prisonerIsAwake == true && knightIsAwake == false && archerIsAwake == false) 
+        {
+            return true;
+        }
+
+        return false;
     }
 }
