f7cd662253bb775e87c7ed6a4f05ecf8a5f1ad36
GilGuereque <gguereque326@gmail.com>
2024-05-15 20:08:01 -0700

    annalyns infiltration

--- csharp/annalyns-infiltration/obj/Release/net8.0/AnnalynsInfiltration.AssemblyInfo.cs
+++ csharp/annalyns-infiltration/obj/Release/net8.0/AnnalynsInfiltration.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnnalynsInfiltration")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f19ce38bfa1b44910f40147ab6e0391320067b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnnalynsInfiltration")]
