c52b5f73293c009ec66d5645f25d8bb7e33bfdc1
GilGuereque <gguereque326@gmail.com>
2024-05-15 20:27:43 -0700

    log levels exercise

---
+++ csharp/log-levels/LogLevels.cs
@@ -0,0 +1,20 @@
+using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        // throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        return LogLine.Message("[ERROR]: Invalid operation");
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+    }
+
+    public static string Reformat(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+    }
+}
